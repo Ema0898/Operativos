8ff193f2fefa53bc8f8b1d31c1f8982fe0cf1fa4
bbadilla <bbadilla17@hotmail.com>
2020-07-23 22:23:57 -0600

    Add Round Robin

--- Proyecto2/lib/algorithms.c
+++ Proyecto2/lib/algorithms.c
@@ -8,23 +8,38 @@
 
 void bridge_list_update(algs_params *params) {
   struct timeval tic, toc;
-  float time_difference; 
+  float time_difference, time_difference2; 
   int list_size = llist_get_size(params->bridge);
   alien *alien_now;
 
   for(int i = 0; i < list_size; i++) {
     alien_now = (alien*)llist_get_by_index(params->bridge, i);
     gettimeofday(&toc, NULL);
-    tic = alien_now -> last_update;
+    tic = alien_now->last_update;
     time_difference = (double)(toc.tv_usec - tic.tv_usec) / 1000000 + (double)(toc.tv_sec - tic.tv_sec);
     alien_now->last_update = toc;
     alien_now->accumulator += time_difference;
     alien_now->progress = alien_now->accumulator/alien_now->duration;
     if(alien_now->progress >= 1) {
-      printf("Termino de pasar el que faltaba\n");
+      printf("Termino de pasar un alien\n");
       *params->weight_now -= alien_now->weight; 
       llist_remove_by_index(params->bridge, i);
       break;
+    } else {
+      if(params->bridge_struct->bridge_type == ROUND_ROBIN) {
+        time_difference2 = (double)(alien_now->last_update.tv_usec - alien_now->work_init_time.tv_usec) / 1000000 + (double)(alien_now->last_update.tv_sec - alien_now->work_init_time.tv_sec);
+        if(params->bridge_struct->quantum <= time_difference2){
+          printf("QUANTUM OVER!!!!\n");
+          llist_remove_by_index(params->bridge, i);
+          *params->weight_now -= alien_now->weight; 
+          if(params->turn == 0) {
+            llist_insert_end(params->north, alien_now);
+          } else {
+            llist_insert_end(params->south, alien_now);
+          }
+          break;
+        }  
+      }
     }
   }
 } 
@@ -53,14 +68,17 @@ void Y_algorithm(algs_params *params){
         alien_to_move = (alien*)llist_pop(params->south);
       }
       while(*params->weight_now + alien_to_move->weight > params->bridge_weight) {
+        params->turn = turn;
         bridge_list_update(params);
       }
       *params->weight_now += alien_to_move->weight;
       alien_to_move->working = 1;
       gettimeofday(&alien_to_move->last_update, NULL);
+      gettimeofday(&alien_to_move->work_init_time, NULL);
       llist_insert_end(params->bridge, alien_to_move);
     }
     while(llist_get_size(params->bridge) > 0){
+      params->turn = turn;
       bridge_list_update(params);
     }
 
@@ -81,7 +99,7 @@ void semaphore_algorithm(algs_params *params){
   struct timeval tic2, toc2;
   float time_difference2, accumulator;
 
-  float sem_north_time = 2, sem_south_time = 4;
+  float sem_north_time = 5, sem_south_time = 10;
 
   params->amount_to_pass = 1;
 
@@ -98,17 +116,24 @@ void semaphore_algorithm(algs_params *params){
         if(llist_get_size(params->north) == 0) {
           break;
         };
-        alien_to_move = (alien*)llist_pop(params->north);
+        alien_to_move = (alien*)llist_get_by_index(params->north, 0);
       } else {
         if(llist_get_size(params->south) == 0) {
           break;
         };
-        alien_to_move = (alien*)llist_pop(params->south);
+        alien_to_move = (alien*)llist_get_by_index(params->south, 0);
       }
       if(*params->weight_now + alien_to_move->weight <= params->bridge_weight) {
+        if(turn == 0) {
+          llist_pop(params->north);
+        } else {
+          llist_pop(params->south);
+        }
         *params->weight_now += alien_to_move->weight;
         alien_to_move->working = 1;
         gettimeofday(&alien_to_move->last_update, NULL);
+        gettimeofday(&alien_to_move->work_init_time, NULL);
+        printf("Inserta a la lista!\n");
         llist_insert_end(params->bridge, alien_to_move);
       }
     }
@@ -117,23 +142,26 @@ void semaphore_algorithm(algs_params *params){
     time_difference2 = (double)(toc2.tv_usec - tic2.tv_usec) / 1000000 + (double)(toc2.tv_sec - tic2.tv_sec);
     accumulator += time_difference2;
 
+    params->turn = turn;
     bridge_list_update(params);
 
     if(turn == 0) {
       if(accumulator >= sem_north_time) {
-        printf("Cambio de lado al sur\n");
         while(llist_get_size(params->bridge) > 0){
+          params->turn = turn;
           bridge_list_update(params);
         }
+        printf("Cambio de lado al sur\n");
         turn = !turn;
         accumulator = 0;
       }
     } else {
       if(accumulator >= sem_south_time) {
-        printf("Cambio de lado al norte\n");
         while(llist_get_size(params->bridge) > 0){
+          params->turn = turn;
           bridge_list_update(params);
         }
+        printf("Cambio de lado al norte\n");
         turn = !turn;
         accumulator = 0;
       }
--- Proyecto2/src/main2.c
+++ Proyecto2/src/main2.c
@@ -11,13 +11,14 @@
 
 int main(){
   llist* aliens = llist_create(NULL);
-  llist* bridge = llist_create(NULL);
+  llist* bridge_list = llist_create(NULL);
+  bridge* bridge_struct = (bridge*)malloc(sizeof(bridge));
 
   alien* new_alien =  (alien*)malloc(sizeof(alien));
   gettimeofday(&new_alien->work_init_time, NULL);
   new_alien->last_update = new_alien->work_init_time;
   new_alien->progress = 50;
-  new_alien->duration = 2;
+  new_alien->duration = 10;
   new_alien->working = 4;
   new_alien->accumulator = 0;
   new_alien->priority = 0;
@@ -28,7 +29,7 @@ int main(){
   gettimeofday(&new_alien2->work_init_time, NULL);
   new_alien2->last_update = new_alien2->work_init_time;
   new_alien2->progress = 50;
-  new_alien2->duration = 2;
+  new_alien2->duration = 10;
   new_alien2->working = 5;
   new_alien2->accumulator = 0;
   new_alien2->priority = 0;
@@ -39,7 +40,7 @@ int main(){
   gettimeofday(&new_alien3->work_init_time, NULL);
   new_alien3->last_update = new_alien3->work_init_time;
   new_alien3->progress = 50;
-  new_alien3->duration = 2; 
+  new_alien3->duration = 10; 
   new_alien3->working = 5;
   new_alien3->accumulator = 0;
   new_alien3->priority = 1;
@@ -53,14 +54,19 @@ int main(){
   // lottery_winner(aliens);
 
   int *weight_now = (int*)malloc(sizeof(int));
+  bridge_struct->weight = 20;
+  bridge_struct->length = 50;
+  bridge_struct->bridge_type = ROUND_ROBIN;
+  bridge_struct->quantum = 6;
   *weight_now = 0;
   algs_params* params = (algs_params*)malloc(sizeof(algs_params));
   params->north = aliens;
   params->south = aliens;
-  params->bridge = bridge;
+  params->bridge = bridge_list;
   params->amount_to_pass = 5;
   params->bridge_weight = 20;
-  params->weight_now = weight_now; 
+  params->weight_now = weight_now;
+  params->bridge_struct = bridge_struct;
   
   semaphore_algorithm(params);
 
